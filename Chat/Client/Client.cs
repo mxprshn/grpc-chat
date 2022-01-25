d510706f3a844f7e6b45a060248cfaa31ef7d3e3
Nikita Mamro <nikita.mamro@compvisionsys.com>
2022-01-25 16:23:45 +0300

    Added experimental project with grpc

---
+++ Chat/Client/Client.cs
@@ -0,0 +1,75 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chat.Client
+{
+    public class Client : BackgroundService
+    {
+        private readonly ILogger<Client> _logger;
+        private readonly string _url;
+
+        public Client(ILogger<Client> logger)
+        {
+            _logger = logger;
+            _url = "http://localhost:5000";
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var name = "John Wick";
+
+            using var channel = GrpcChannel.ForAddress(_url);
+            var client = new Chat.ChatClient(channel);
+
+            using var streaming = client.SendMessage(new Metadata
+            {
+                new Metadata.Entry("name", name)
+            });
+
+            var response = Task.Run(async () =>
+            {
+                while (await streaming.ResponseStream.MoveNext())
+                {
+                    Console.WriteLine($"{streaming.ResponseStream.Current.Name}: {streaming.ResponseStream.Current.Text}");
+                }
+            });
+
+            Console.Write("John's input: ");
+            var line = Console.ReadLine();
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await streaming.RequestStream.WriteAsync(new ChatMessage
+                {
+                    Time = Timestamp.FromDateTimeOffset(DateTimeOffset.UtcNow),
+                    Name = name,
+                    Text = line
+                });
+
+                Console.Write("John's input: ");
+                line = Console.ReadLine();
+                DeletePrevConsoleLine();
+            }
+
+            await streaming.RequestStream.CompleteAsync();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static void DeletePrevConsoleLine()
+        {
+            if (Console.CursorTop == 0) return;
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+        }
+    }
+}
---
+++ Chat/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Chat.Client;
+using Microsoft.Extensions.Logging;
+
+namespace Chat
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        // Additional configuration is required to successfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureServices(svc =>
+                {
+                    svc.AddHostedService<Client.Client>();
+                })
+                .ConfigureLogging((context, logging) =>
+                {
+                    logging.ClearProviders();
+                });
+    }
+}
